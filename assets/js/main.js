028ad3d1cf1fa8bc3bbb72582f16ebc8025f61f0
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-07-25 16:19:33 -0500

    Impresion de los 10 task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,89 @@
+var o = 0;
 
-var i = 0;
+//array para guardar las tareas
 
-array para guardar las tareas
+function addTask (mensaje){
+	var lista = document.getElementById("lista");
+	var string = "";
+	var style = "";
+	style += "<form action='#''>";
+	style += "<p><input type='checkbox' id='test5'/>";
+	style += "<label for='test5'>"+ mensaje +"</label></p></form>"; 
+	string +="<div id='nuevo'>" +style+"</div>"
+	lista.innerHTML += string;
+	console.log(mensaje);
+}
 
 
-[
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 10,
-    "title": "illo est ratione doloremque quia maiores aut",
-    "completed": true
-  },
-  {
-    "userId": 1,
-    "id": 11,
-    "title": "vero rerum temporibus dolor",
-    "completed": true
-  },
+var initialTask =[
+	  {
+	    "userId": 1,
+	    "id": 1,
+	    "title": "Delectus aut autem",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 2,
+	    "title": "Quis ut nam facilis et officia qui",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 3,
+	    "title": "Fugiat veniam minus",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 4,
+	    "title": "Et porro tempora",
+	    "completed": true
+	  },
+	  {
+	    "userId": 1,
+	    "id": 5,
+	    "title": "Laboriosam mollitia et enim quasi adipisci quia provident illum",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 6,
+	    "title": "Qui ullam ratione quibusdam voluptatem quia omnis",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 7,
+	    "title": "Illo expedita consequatur quia in",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 8,
+	    "title": "Quo adipisci enim quam ut ab",
+	    "completed": true
+	  },
+	  {
+	    "userId": 1,
+	    "id": 9,
+	    "title": "Molestiae perspiciatis ipsa",
+	    "completed": false
+	  },
+	  {
+	    "userId": 1,
+	    "id": 10,
+	    "title": "Illo est ratione doloremque quia maiores aut",
+	    "completed": true
+	  },
+	  {
+	    "userId": 1,
+	    "id": 11,
+	    "title": "Vero rerum temporibus dolor",
+	    "completed": true
+	  },
 ]
+
+for(var i in initialTask){
+	addTask(initialTask[i].title);
+}
